76cb38ac4773b5498c49e970627ffe19c07d856e
0einstein0 <fzulfiqar.bscs18seecs@seecs.edu.pk>
2021-12-29 00:42:27 +0500

    Upload

--- src/components/common/CoreValuesBlock/index.js
+++ src/components/common/CoreValuesBlock/index.js
@@ -3,11 +3,11 @@ import * as styles from "./values.module.scss";
 
 const CoreValuesBlock = ({ icon, title, midVal }) => {
 	return (
-		<div className={`text-center mdiv  ${midVal ? styles.midVal : ""} `}>
+		<div className={`text-center   ${midVal ? styles.midVal : ""} `}>
 			<div className={styles.valIcon}>
 				<img src={icon} alt="" className="img-fluid" />
 			</div>
-			<p>{title}</p>
+			<p className={styles.valText}>{title}</p>
 		</div>
 	);
 };
