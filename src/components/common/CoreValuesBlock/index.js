45ff83fa9191e15d24e096454ea27197125a4466
0einstein0 <fzulfiqar.bscs18seecs@seecs.edu.pk>
2021-12-29 00:34:10 +0500

    Upload

--- src/components/common/CoreValuesBlock/index.js
+++ src/components/common/CoreValuesBlock/index.js
@@ -7,7 +7,7 @@ const CoreValuesBlock = ({ icon, title, midVal }) => {
 			<div className={styles.valIcon}>
 				<img src={icon} alt="" className="img-fluid" />
 			</div>
-			<h4>{title}</h4>
+			<h3>{title}</h3>
 		</div>
 	);
 };
